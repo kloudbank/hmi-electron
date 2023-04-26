286c1882a49173f5d0aacbacb37a315ef178268d
nogada <seungil.han@gmail.com>
2023-04-26 12:37:47 +0900

    electron initialize

---
+++ index.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow=()=>{
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+      })
+      win.loadURL("https://www.naver.com")
+}
+
+app.whenReady().then(() => {
+    createWindow()
+  })
