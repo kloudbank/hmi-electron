5a4962a8bb98293280ea3a2f5ccf8fda56d20395
noisonnoiton <noisonnoiton@gmail.com>
2023-05-02 10:20:08 +0900

    add dotenv, config

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { app, BrowserWindow, screen } = require('electron')
 
 const createWindow=()=>{
@@ -5,7 +6,9 @@ const createWindow=()=>{
     const win = new BrowserWindow({
         width,height
       })
-      win.loadURL("http://127.0.0.1:3000")
+      // win.loadURL("http://127.0.0.1:3000")
+      // win.loadURL("http://hmi-web.edge.kubepia.net")
+      win.loadURL(process.env.HMI_WEB_URL)
 }
 
 app.whenReady().then(() => {
